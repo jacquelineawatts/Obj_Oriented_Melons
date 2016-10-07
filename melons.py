a100acffc893dfa54251ab9ef8777cd025df87ba
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-06 17:26:28 -0700

    Add flat fee of  to international orders w less than 10 melons.

--- melons.py
+++ melons.py
@@ -2,15 +2,14 @@
 
 class AbstractMelonOrder(object):
 
-    def __init__(self, species, qty, country_code = None):
+    def __init__(self, species, qty, tax=None, order_type=None):
         """Initialize melon order attributes"""
 
         self.species = species
         self.qty = qty
         self.shipped = False
-        self.country_code = country_code
-        self.order_type = None
-        self.tax = None
+        self.order_type = order_type
+        self.tax = tax
 
 
     def mark_shipped(self):
@@ -33,9 +32,7 @@ class DomesticMelonOrder(AbstractMelonOrder):
     def __init__(self, species, qty):
         """Initialize melon order attributes"""
 
-        super(DomesticMelonOrder,self).__init__(species, qty)
-        self.order_type = "domestic"
-        self.tax = 0.08
+        super(DomesticMelonOrder,self).__init__(species, qty, 0.08, "domestic")
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
@@ -44,13 +41,21 @@ class InternationalMelonOrder(AbstractMelonOrder):
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes"""
 
-        super(InternationalMelonOrder,self).__init__(species, qty, country_code)
-        self.order_type = "international"
-        self.tax = 0.17
-
+        super(InternationalMelonOrder,self).__init__(species, qty, 0.17, 'international')
+        self.country_code = country_code
 
 
     def get_country_code(self):
         """Return the country code."""
 
         return self.country_code
+
+    def get_total(self):
+        original_total = super(InternationalMelonOrder, self).get_total()
+        print original_total
+        if self.qty < 10:
+            new_total = original_total + 3
+            print new_total
+            return new_total
+        else:
+            return original_total
